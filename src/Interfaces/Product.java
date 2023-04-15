9068bb14c545e41a7f3682e3ee7a47253f933443
glnsb <marcusorhan@gmail.com>
2023-04-15 03:50:01 -0400

    Polymorphism
    book, customer classes added
    Product interface added

---
+++ src/Interfaces/Product.java
@@ -0,0 +1,12 @@
+package Interfaces;
+
+public interface Product {
+    double getPrice();
+    void setPrice(double price);
+
+    String getName();
+    void setName(String name);
+
+    String getColor();
+    void setColor(String color);
+}
